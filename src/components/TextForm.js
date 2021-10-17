602bc9252a74a5a9995a90fcd331202c6a43458e
sanp9 <patel.san360@gmail.com>
2021-10-17 17:40:24 +0530

    SEO Update

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,13 +2,9 @@ import React, { useState } from 'react';
 
 export default function TextForm(props) {
     const [text, setText] = useState('');
-<<<<<<< HEAD
     let timeToRead = 0.008 * text.split(" ").filter((element) => {
         return (element.length !== 0);
     }).length;
-=======
-    let timeToRead = 0.008 * text.split(" ").length;
->>>>>>> 8bf5767d4f16024031375c8c81f8801e3ba11d97
 
     // state can be changed only by set function
     function setCharAt(str, index, chr) {
@@ -96,7 +92,6 @@ export default function TextForm(props) {
             <div className='container my-2' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-<<<<<<< HEAD
                     <textarea className="form-control" id="myBox" rows="8" value={text} style={{ backgroundColor: props.mode === 'dark' ? '#13466E' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} onChange={handleOnChange} ></textarea>
                 </div>
                 <div>
@@ -106,17 +101,6 @@ export default function TextForm(props) {
                     <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
                     <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
                     <button className="btn btn-primary mx-1 my-1" onClick={handleCTClick}>Clear text</button>
-=======
-                    <textarea className="form-control" id="myBox" rows="8" value={text} style={{ backgroundColor: props.mode === 'dark' ? '#343a40' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} onChange={handleOnChange} ></textarea>
-                </div>
-                <div>
-                    <button className="btn btn-primary mx-1 " onClick={handleUpClick}>Convert to Upper Case</button>
-                    <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lower Case</button>
-                    <button className="btn btn-primary mx-1" onClick={handleCamelClick}>Convert to Camel Case</button>
-                    <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-                    <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-                    <button className="btn btn-primary mx-1" onClick={handleCTClick}>Clear text</button>
->>>>>>> 8bf5767d4f16024031375c8c81f8801e3ba11d97
                 </div>
                 {/* <h1></h1>
                 <button className="btn btn-primary mx-1" onClick={handleConHClick}>Convert English to Hindi</button>
@@ -125,13 +109,9 @@ export default function TextForm(props) {
             </div>
             <div className="container my-5" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Your text summary</h2>
-<<<<<<< HEAD
                 <p>{text.split(" ").filter((element) => {
                     return (element.length !== 0);
                 }).length} words and {text.length} characters</p>
-=======
-                <p>{text.split(" ").length} words and {text.length} characters</p>
->>>>>>> 8bf5767d4f16024031375c8c81f8801e3ba11d97
                 <p>{timeToRead} Minutes time to read text</p>
 
             </div>
