8bf5767d4f16024031375c8c81f8801e3ba11d97
sanp9 <patel.san360@gmail.com>
2021-10-17 12:14:35 +0530

    New UI

---
+++ src/components/About.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+
+export default function About(props) {
+
+    const myStyle = {
+        backgroundColor: props.mode === 'dark' ? '#343a40' : 'white', color: props.mode === 'dark' ? 'white' : 'black'
+    }
+
+    return (
+        <div className="container" style={myStyle}>
+            <h2 className="my-2">About Xenator</h2>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={myStyle}>
+                            Xenator
+                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample" style={myStyle}>
+                        <div className="accordion-body">
+                            This Website gives you different feautres for interacting with your text and giving substantial knowledge about the text you want to analyze.It provides instant character count - Word count - Total Read time and more..
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={myStyle}>
+                            Browser Compatible
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample" style={myStyle}>
+                        <div className="accordion-body">
+                            This software works in every browser and compatible with all devices and provides all the utilities to be used in analyzing texts from blogs , excel , pdf , essays ,etc.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={myStyle}>
+                            About Creater
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample" style={myStyle}>
+                        <div className="accordion-body">
+                            <strong>Sankalp Patel  --> <a href="https://github.com/sankalp360">https://github.com/sankalp360</a></strong>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <button onClick={toggleStyle} type="button" className="btn btn-primary my-2">{btntext}</button> */}
+        </div>
+    )
+}
---
+++ src/components/TextForm.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+
+export default function TextForm(props) {
+    const [text, setText] = useState('');
+    let timeToRead = 0.008 * text.split(" ").length;
+
+    // state can be changed only by set function
+    function setCharAt(str, index, chr) {
+        if (index > str.length - 1) return str;
+        return str.substring(0, index) + chr + str.substring(index + 1);
+    }
+
+    const handleOnChange = (event) => {
+        // console.log('On Change')
+        setText(event.target.value);
+    }
+
+    const handleUpClick = () => {
+        // console.log('Uppercase was clciked');
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert('Converted to Uppercase', 'success');
+    }
+    const handleLowClick = () => {
+        // console.log('Uppercase was clciked');
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert('Converted to Lowercase', 'success');
+
+    }
+    const handleCamelClick = () => {
+        // console.log('Uppercase was clciked');
+        let newText = text.toLowerCase();
+        setText(newText);
+        let curr = newText;
+
+        for (let i = 0; i < newText.length - 1; i++) {
+
+            if (i === 0) {
+                curr = setCharAt(curr, i, curr.charAt(i).toUpperCase());
+            }
+            if (curr.charAt(i) === ' ') {
+                curr = setCharAt(curr, i + 1, curr.charAt(i + 1).toUpperCase());
+            }
+            setText(curr);
+        }
+
+        props.showAlert('Converted to Camelcase', 'success');
+
+    }
+
+    const handleCopy = () => {
+        let newText = text;
+        navigator.clipboard.writeText(newText);
+        props.showAlert('Text Copied to Clipboard', 'success');
+
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert('Extra Spaces Removed', 'success');
+
+    }
+    const handleConHClick = () => {
+        // console.log('Uppercase was clciked');
+        // let options = {
+        //     sourceLanguage:
+        //         google.elements.transliteration.LanguageCode.ENGLISH,
+        //     destinationLanguage:
+        //         [google.elements.transliteration.LanguageCode.HINDI],
+        //     shortcutKey: 'ctrl+g',
+        //     transliterationEnabled: true
+        // };
+
+        // let control = new
+        //     google.elements.transliteration.TransliterationControl(options);
+        // control.makeTransliteratable(["txtEnglish"]);
+        // setText(control);
+    }
+
+    const handleCTClick = () => {
+        // console.log('Uppercase was clciked');
+        let newText = "";
+        setText(newText);
+        props.showAlert('Text Cleared', 'success');
+    }
+    return (
+        <>
+            <div className='container my-2' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" id="myBox" rows="8" value={text} style={{ backgroundColor: props.mode === 'dark' ? '#343a40' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} onChange={handleOnChange} ></textarea>
+                </div>
+                <div>
+                    <button className="btn btn-primary mx-1 " onClick={handleUpClick}>Convert to Upper Case</button>
+                    <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lower Case</button>
+                    <button className="btn btn-primary mx-1" onClick={handleCamelClick}>Convert to Camel Case</button>
+                    <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+                    <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                    <button className="btn btn-primary mx-1" onClick={handleCTClick}>Clear text</button>
+                </div>
+                {/* <h1></h1>
+                <button className="btn btn-primary mx-1" onClick={handleConHClick}>Convert English to Hindi</button>
+                <button className="btn btn-primary mx-1" onClick={handleConHClick}>Convert Hindi to English</button> */}
+
+            </div>
+            <div className="container my-5" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{timeToRead} Minutes time to read text</p>
+
+            </div>
+            <div className="container my-5" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <h2>Preview</h2>
+                <p>{text.length > 0 ? text : "Enter Something in Text Box to Preview"}</p>
+            </div>
+        </>
+    )
+}
