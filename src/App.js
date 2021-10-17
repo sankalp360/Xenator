ad2cf29fd09479c96dfa57b441c0fb19de7ac614
sanp9 <patel.san360@gmail.com>
2021-10-17 12:14:35 +0530

    New UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+// function based component 
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    // type - it is the type of alert you want to send in Bootstrap.
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1200);
+  }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert('Dark mode has been enabled', 'success');
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light mode has been enabled', 'success');
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+      <Navbar title="Xenator" aboutText="About" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-5">
+        <Switch>
+            <Route exact path="/about">
+              <About mode={mode} />
+            </Route>
+            <Route exact path="/">
+        <TextForm showAlert={showAlert} heading="Enter the text below to analyze:" mode={mode} />
+        </Route>
+          </Switch>
+
+      </div>
+      </Router>
+    </>
   );
 }
 
